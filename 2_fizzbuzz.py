112fcd255d41699596da450462e2142c953ac3bd
twell-star <novikov.igor@mail.ru>
2021-02-26 18:58:38 +0300

    Version 1.3

--- 2_fizzbuzz.py
+++ 2_fizzbuzz.py
@@ -1,2 +1,13 @@
 # Решение задачи #2 - FizzBuzz
 
+start_value = 1000       # начальное значение диапазона 
+end_value = 20000        # конечное значение диапазон
+
+sum_fizzbuzz = 0         # сумма искомых значений 'fizzbuzz'
+
+for i in range(start_value, end_value+1):
+    if (i % 3) == 0 and (i % 5) == 0:
+        sum_fizzbuzz = sum_fizzbuzz + i                     
+
+print(f'Сумма значений FizzBuzz в заданном диапазоне: {sum_fizzbuzz}')    
+        
